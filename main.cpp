8a1a6259ce63a21d4e8e87c66f01301510cc2806
Garrett <emrickgarrett@gmail.com>
2013-08-28 20:24:02 -0400

    Got rid of all the explanation comments to get rid of some of the clutter.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
-using namespace std;
+using namespace std; // so I don't have to keep typing out std::
 
-//1. Create a function, named "prime", which tests an
-// integer, n, to see if it is prime. It should return a bool. 
-// 
-// You don't have to do
-// anything tricky. Just try modding (%) it by every number
-// between 2 and n-1. If it never gives 0, then it is prime.
-// Special case: Only numbers 2 or greater can be prime.
-// Example inputs:
-// prime(1) --> false
-// prime(-7) --> false
-// prime(2) --> true
-// prime(4) --> false
-//
-//Hints: This exercise uses for loops, if statements,
-// and the % operator.
-
-//TODO: Declare and implement "prime" function here
 bool prime(int num){
 	if(num < 2) return false;
 
@@ -44,21 +27,8 @@ void testPrime(){
   std::cout << "testPrime: SUCCESS" << std::endl;
 }
 
-//2. Create a function, name "defix", which takes in a string and
-//   returns a string. If the string starts with a pre-fix attached
-//   by a dash, strip off the prefix and the dash. Otherwise, return
-//   the string unchanged. If there is more than one prefix, remove only
-//   the first one. Note that the input and output should both be type 
-//   std::string
-//
-//Example inputs:
-// defix("re-run") --> "run"
-// defix("pre--text") --> "-text"
-// defix("-ooh") --> "ooh"
-// defix("moo") --> "moo"
-//
-//Hints: Check out the string API documentation at http://www.cplusplus.com/reference/string/string/
-// The find functions and substr function will be easiest.
+
+
 
 //TODO: declare and implement "defix" function here
 string defix(string input){
@@ -71,6 +41,7 @@ string defix(string input){
 
 	return input;
 }
+
 
 //This is a basic tester for "defix"
 void testDefix(){
@@ -87,21 +58,7 @@ void testDefix(){
   std::cout << "testDefix: SUCCESS" << std::endl;
 }
 
-//3. Create a function called "sumSlice" that takes 3 inputs. The first is
-//   an array of integers, the second is an integer "s" that represents the
-//   starting index, and the 3rd is an int "len" that represents the length. 
-//   Sum up all entries in the array, starting with item "s" and ending with
-//   s+len-1, and return the sum. s and len must both be >= 0. If not, return 0
-//   Note: You may assume that the array is at least s+len items long. If not,
-//   it is okay to crash or return unexpected results.
-//
-//Example inputs:
-//sumSlice({1,2,3,4},1,1) --> 2
-//sumSlice({1,2,3,4},1,3) --> 9
-//sumSlice({1,-1,1,-1},0,4) --> 0
-//sumSlice({1,2,3,4},1,0) --> 0
-//
-//Hints: Your answer is going to be very similar to what you would do in Java
+
 
 //TODO: Declare and implement sumSlice here
 int sumSlice(int arrayToSum[], int startingIndex, int endingIndex){
@@ -145,38 +102,11 @@ void testSumSlice(){
   std::cout << "testSumSlice: SUCCESS" << std::endl;
 }
 
-//4. Create a function called "square" which takes an int, n, as input,
-//   but returns no output (so return type will be "void")
-//   The function should use the std::cout object to print a square to the
-//   screen that is n-by-n, with a border made of -, + and |, and the inside
-//   filled with o
-//   If n is <= 0, do nothing.
-//
-//Examples:
-// square(5) should print:
-// +---+
-// |ooo|
-// |ooo|
-// |ooo|
-// +---+
-// square(2) should print:
-// ++
-// ++
-// square(1) should print:
-// +
-// square(3) should print:
-// +-+
-// |o|
-// +-+
-//
-//Note/warning: I did not write an automated tester for this one. Please
-// test it however you can, to try to make sure it does the right thing for
-// all possible inputs.
+
 
 //Need to declare/initialize the function so I don't get errors.
 void printEdge(int n);
 
-//TODO: Declare and implement "square" function here
 void square(int n){
 	if(n <= 0) return;
 
@@ -223,18 +153,8 @@ void printEdge(int n){
 }
 
 
-//5. Create a function called listPrimes which takes an int, n, as input.
-//   It should use "new" to allocate an array of length n, and then put
-//   the first n prime numbers into it, in order. You should re-use your
-//   prime method here.
-//   Note: The return type of the method should be int*, 
-//
-//Example outputs:
-// listPrimes(5) should return an array containing {2, 3, 5, 7, 11}
-//
-//Hint: While loops work better than for loops for this one.
 
-//TODO: Declare and implement listPrimes here
+
 int* listPrimes(int n){
 
 	int* primeArray = new int[n];
