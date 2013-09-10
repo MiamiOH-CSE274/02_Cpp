b83f088a8432d9974e630e23e084019a8d2cc1f3
Patrick Proctor <proctopj@miamioh.edu>
2013-09-10 12:08:28 -0400

    I fixed all remaining errors including a single = operator in a boolean argument which had been throwing off my "prime" method results. I also optimized the "prime" method.

--- main.cpp
+++ main.cpp
@@ -31,15 +31,16 @@ above, this logical disjointment could lead to future misinterpretations of othe
 bool prime(int x){
 	if(x<2)
 		return false;
-	if(x=2)
+	if(x==2) //always triple check your operators in arguments... damn double vs. single equals... Why does single equals even compile here???!!!
 		return true;
-	int limit = x/2;  //I am not entirely sure how the control argument within a for loop works. If I substituted in x/2, does the program calculate this for each iteration of the loop or not? I could not find this information readily available online.
+	int limit = sqrt(x);  //I am not entirely sure how the control argument within a for loop works. The square root limit can be proven through some nasty induction. there is no need to test beyond this value.
 	for(int i=2; i<=limit; i++){  //noting the inherrent inaccuracy of integer division, using the <= operator negates any possible missing values
-		if(x%i == 0)
+		if(x % i == 0)
 			return false;
 	}
 	return true;  //method should only arrive here if all tested values fail to produce x%i == 0.
 }
+
 
 
 //This is a basic tester for the "prime" function
