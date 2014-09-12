79e9b32e5db4acf63f6e3fcbb476044c19c105fb
XCCOOP <country043@frontier.com>
2014-09-11 22:01:28 -0400

    Fixed 'Prime' function, added non-working 'List Primes'

--- main.cpp
+++ main.cpp
@@ -24,26 +24,20 @@
 
 bool prime(int n) {
 
-	if(n == 2) {
-		return true;
-	}
-
-	else if(n<2) {
+	if(n<2){
 		return false;
 	}
-
-	else if(n>2) {
-		for(int i=n-1; i>2; i--) {
-	
-			if(n%i == 0) {
+	if(n==2){
+		return true;
+	}
+	if(n>2){
+		for(int i=2; i<n-1; i++){
+			if(n%i==0){
 				return false;
 			}
-	
 		}
 	}
-
 	return true;
-
 }
 
 //This is a basic tester for the "prime" function
@@ -239,7 +233,23 @@ void square(int n) {
 
 //TODO: Declare and implement listPrimes here
 
-/**
+int* listPrimes(int n) {
+
+	int primeArraySize = n;
+	int* primeArray = new int[primeArraySize];
+	int i=0;
+	while(i != primeArraySize) {
+	
+		int testPrime = 2;
+		if(prime(testPrime)){
+			primeArray[i] = testPrime;
+			i++;
+		}
+		testPrime++;
+	}
+	return primeArray;
+}
+
 
 void testListPrimes(){
   int some_primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
@@ -259,14 +269,13 @@ void testListPrimes(){
   std::cout << "testListPrimes: SUCCESS" << std::endl;
 }
 
-**/
 
 int main(){
   testPrime();
   testDefix();
   testSumSlice();
   square(7);
-  //testListPrimes();
+  testListPrimes();
 
   return 0;
 }
